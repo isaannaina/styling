212454b0d94b7f68eb0a596e4fce82cb6d0f775c
piyush <sainipiyus11111@gmail.com>
2023-03-11 16:26:13 +0530

    code202

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
-import AddUser from './component/Users/AddUser';
-import UserList from './component/Users/UserList';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const addUserHandler = (name, age) => {
-    setUsersList((prevUsersList) => {
-      return [...prevUsersList, { name, age, id: Math.random().toString() }];
-    });
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
   };
 
-  const deleteUserHandler = (id) => {
-    setUsersList((prevUsersList) => {
-      return prevUsersList.filter((user) => user.id !== id);
-    });
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
   };
 
   return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UserList users={usersList} onDelete={deleteUserHandler} />
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
 export default App;
-
