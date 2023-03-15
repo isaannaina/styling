e1e2aeebbbd4efdf6f022b690903bfab3887ecec
piyush <sainipiyus11111@gmail.com>
2023-03-15 21:08:06 +0530

    adding summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import {Fragment} from 'react';
 import Header from './Components/Header/Header'
+import Meals from './Components/Meals/Meals';
+
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header/>
-    </div>
+      <main>
+        <Meals/>
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/Meals/Meals.js
@@ -0,0 +1,10 @@
+import {Fragment}from "react";
+import MealsSummary from "./MealsSummary";
+const Meals =()=>{
+return(
+     <Fragment>
+ <MealsSummary/>
+ </Fragment>
+)
+}
+export default Meals;
