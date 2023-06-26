eee3b9f666e88dc55e4e543bce6e450309beac2f
piyush <sainipiyus11111@gmail.com>
2023-06-26 09:27:09 +0530

    productAhowCase

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
 
+import ProductShowCase from './component/ProductShowCase';
 
-import React, { useState, createContext } from 'react';
-import Header from './component/Header';
-import Item from './component/Item';
-import Cart from './component/Cart';
-
-export const CartContext = createContext();
 
 const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
-  const addToCart = (item) => {
-    setCartItems((prevCartItems) => [...prevCartItems, item]);
-  };
-
-
-
-  const toggleCart = () => {
-    setIsCartOpen(!isCartOpen);
-  };
-
   return (
     <div>
-      <CartContext.Provider value={cartItems}>
-        <Header cartItems={cartItems} toggleCart={toggleCart} />
-        <Item
-          name="T-Shirt 1"
-          description="winter t shirt"
-          price={19.99}
-          addToCart={addToCart}
-        />
-        <Item
-          name="T-Shirt 2"
-          description="galli boy t shirt."
-          price={24.99}
-          addToCart={addToCart}
-        />
-        <Item
-          name="T-Shirt 3"
-          description="sky hig t shirt ."
-          price={29.99}
-          addToCart={addToCart}
-        />
-        {isCartOpen && <Cart cartItems={cartItems} onClose={toggleCart} />}
-      </CartContext.Provider>
+      <ProductShowCase></ProductShowCase>
     </div>
   );
 };
---
+++ src/components/ProductShowCase.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const ProductShowCase = () => {
+  const productsArr = [
+    {
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    },
+    {
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    },
+    {
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    },
+    {
+      title: 'Blue Color',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    },
+  ];
+
+  return (
+    <div>
+      {productsArr.map((product, index) => (
+        <div key={index} className="product">
+          <img src={product.imageUrl} alt={product.title} />
+          <h3>{product.title}</h3>
+          <p>${product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductShowCase;
