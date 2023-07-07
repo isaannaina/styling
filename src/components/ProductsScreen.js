73b40bcdc00cf5ef679a9d80dbe9ca0ceba35125
piyush <sainipiyus11111@gmail.com>
2023-07-07 17:57:32 +0530

    adding a form to add movie

--- src/components/ProductsScreen.js
+++ src/components/ProductsScreen.js
@@ -1,5 +1,75 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useState } from 'react';
 import ProductItem from './ProductItem';
+
+const MovieForm = ({ handleAddMovie }) => {
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    openingText: '',
+    releaseDate: ''
+  });
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setNewMovie(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    handleAddMovie(newMovie);
+    setNewMovie({
+      title: '',
+      openingText: '',
+      releaseDate: ''
+    });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={newMovie.title}
+            onChange={handleInputChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="openingText" className="form-label">Opening Text:</label>
+          <input
+            type="text"
+            id="openingText"
+            name="openingText"
+            value={newMovie.openingText}
+            onChange={handleInputChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="releaseDate" className="form-label">Release Date:</label>
+          <input
+            type="text"
+            id="releaseDate"
+            name="releaseDate"
+            value={newMovie.releaseDate}
+            onChange={handleInputChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Add Movie</button>
+      </form>
+    </div>
+  );
+};
 
 const ProductsScreen = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -8,7 +78,7 @@ const ProductsScreen = () => {
   const [retryCount, setRetryCount] = useState(0);
   const [retryIntervalId, setRetryIntervalId] = useState(null);
 
-  const fetchMovieData = useCallback(() => {
+  const fetchMovieData = () => {
     setIsLoading(true);
     setError(null);
     setRetryCount(0);
@@ -26,31 +96,23 @@ const ProductsScreen = () => {
         setError('Something went wrong. Retrying...');
         setRetryIntervalId(setInterval(fetchMovieData, 5000));
       });
-  }, []);
+  };
 
-  const cancelRetry = useCallback(() => {
+  const cancelRetry = () => {
     clearInterval(retryIntervalId);
     setError(null);
-  }, [retryIntervalId]);
+  };
 
-  useEffect(() => {
-    fetchMovieData();
-
-    return () => {
-      clearInterval(retryIntervalId);
-    };
-  }, [fetchMovieData, retryIntervalId]);
-
-  const movieList = useMemo(() => (
-    <div className="product-list">
-      {movies.map((movie, index) => (
-        <ProductItem key={index} movie={movie} />
-      ))}
-    </div>
-  ), [movies]);
+  const handleAddMovie = newMovie => {
+    console.log(newMovie);
+  };
 
   return (
     <div>
+      <MovieForm handleAddMovie={handleAddMovie} />
+      <button onClick={fetchMovieData} className="btn btn-primary">
+        Fetch Movies
+      </button>
       {isLoading ? (
         <div className="loader">Loading...</div>
       ) : error ? (
@@ -60,7 +122,13 @@ const ProductsScreen = () => {
             Cancel
           </button>
         </div>
-      ) : movieList}
+      ) : (
+        <div className="product-list">
+          {movies.map((movie, index) => (
+            <ProductItem key={index} movie={movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
