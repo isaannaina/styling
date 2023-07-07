d9a5779fe926d26b170b4e35fea044c98836c288
piyush <sainipiyus11111@gmail.com>
2023-07-07 17:01:18 +0530

    retring done

--- src/components/ProductsScreen.js
+++ src/components/ProductsScreen.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
-import ProductItem from './ProductItem'
+import React, { useState, useEffect } from 'react';
+import ProductItem from './ProductItem';
+
 const ProductsScreen = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [retryCount, setRetryCount] = useState(0);
+  const [retryIntervalId, setRetryIntervalId] = useState(null);
 
   const fetchMovieData = () => {
     setIsLoading(true);
+    setError(null);
+    setRetryCount(0);
 
     fetch("https://swapi.dev/api/films/")
       .then(response => response.json())
       .then(data => {
         setMovies(data.results);
         setIsLoading(false);
+        setError(null);
       })
       .catch(error => {
         console.log("Error fetching movies:", error);
         setIsLoading(false);
+        setError('Something went wrong. Retrying...');
+        setRetryIntervalId(setInterval(fetchMovieData, 5000));
       });
   };
+
+  const cancelRetry = () => {
+    clearInterval(retryIntervalId);
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(retryIntervalId);
+    };
+  }, [retryIntervalId]);
 
   return (
     <div>
@@ -26,6 +46,13 @@ const ProductsScreen = () => {
       </button>
       {isLoading ? (
         <div className="loader">Loading...</div>
+      ) : error ? (
+        <div>
+          <p>{error}</p>
+          <button onClick={cancelRetry} className="btn btn-danger">
+            Cancel
+          </button>
+        </div>
       ) : (
         <div className="product-list">
           {movies.map((movie, index) => (
