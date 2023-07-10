c265f7e32ebbe5ad4cdfdc250296a312de9a39f7
piyush <sainipiyus11111@gmail.com>
2023-07-10 09:16:18 +0530

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,63 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React, { useState } from 'react';
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
-import { CartProvider } from './component/CartContext';
-import Header from './component/Header';
-import ProductsScreen from './component/ProductsScreen';
-import Cart from './component/Cart';
-import CartPortal from './component/CartPortal';
-import ProductPage from './component/ProductPage';
-import ContactForm from './component/ContactUsPage';
-
-const App = () => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
-  const handleCartToggle = () => {
-    setIsCartOpen(!isCartOpen);
-  };
-
-  const productsArr = [
-    {
-      title: 'Colors',
-      price: 100,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-    },
-    {
-      title: 'Black and white Colors',
-      price: 50,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-    },
-    {
-      title: 'Yellow and Black Colors',
-      price: 70,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-    },
-    {
-      title: 'Blue Color',
-      price: 100,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-    }
-  ];
-
+function App() {
   return (
     <BrowserRouter>
-      <CartProvider>
-        <Header handleCartToggle={handleCartToggle} />
-
-        {isCartOpen && (
-          <CartPortal>
-            <Cart />
-          </CartPortal>
-        )}
-
+      <Layout>
         <Routes>
-          <Route path="/product/:id" element={<ProductPage productsArr={productsArr} />} />
-          <Route path="/contact" element={<ContactForm />} />
-          <Route path="/" element={<ProductsScreen />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Routes>
-      </CartProvider>
+      </Layout>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,98 @@
+import { useState, useRef } from 'react';
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false); // Add isLoading state
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true); // Show loader when submitting the form
+
+    try {
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:${
+          isLogin ? 'signInWithPassword' : 'signUp'
+        }?key=AIzaSyDddUDuA6N4zFOCqXbkYxSOpZJt-q1HHcQ`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Authentication failed!');
+      }
+
+      const data = await response.json();
+      console.log(data); // Handle the response data as per your requirement
+
+      setIsLoading(false); // Hide the loader
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false); // Hide the loader
+    }
+
+    emailInputRef.current.value = ''; // Reset form fields
+    passwordInputRef.current.value = '';
+  };
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading ? (
+            <button type='submit'>
+              {isLogin ? 'Login' : 'Sign Up'}
+            </button>
+          ) : (
+            <p  >Loading...</p>
+          )}
+        </div>
+      </form>
+      {!isLoading && (
+        <button
+          type='button'
+          className={classes.toggle}
+          onClick={switchAuthModeHandler}
+        >
+          {isLogin ? 'Create new account' : 'Login with existing account'}
+        </button>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
