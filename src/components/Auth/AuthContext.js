8484e44223814b1638bea5fef8dadc73775a861e
piyush <sainipiyus11111@gmail.com>
2023-07-11 10:33:53 +0530

    automatically logout

--- src/components/Auth/AuthContext.js
+++ src/components/Auth/AuthContext.js
@@ -1,4 +1,7 @@
-import React, { createContext, useState } from 'react';
+
+
+
+import React, { createContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext({
   token: null,
@@ -6,16 +9,48 @@ const AuthContext = createContext({
   logout: () => {},
 });
 
-const AuthContextProvider = (props) => {
+export const AuthContextProvider = (props) => {
   const [token, setToken] = useState(null);
+  let logoutTimer;
 
   const loginHandler = (token) => {
     setToken(token);
+    const expirationTime = new Date(new Date().getTime() + 5 * 60 * 1000);
+    localStorage.setItem('token', token);
+    localStorage.setItem('expirationTime', expirationTime.toISOString());
+    logoutTimer = setTimeout(logoutHandler, 5 * 60 * 1000);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
   };
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (storedToken && expirationTime) {
+      const remainingTime = new Date(expirationTime) - new Date();
+      if (remainingTime > 0) {
+        setToken(storedToken);
+        logoutTimer = setTimeout(logoutHandler, remainingTime);
+      } else {
+        logoutHandler();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (logoutTimer) {
+        clearTimeout(logoutTimer);
+      }
+    };
+  }, []);
 
   const contextValue = {
     token: token,
@@ -29,8 +64,5 @@ const AuthContextProvider = (props) => {
     </AuthContext.Provider>
   );
 };
+
 export default AuthContext;
-export { AuthContextProvider };
-
-
-
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,64 @@
-import classes from './ProfileForm.module.css';
+import React, { useContext, useState } from 'react';
+import AuthContext from '../Auth/AuthContext';
+import classes from './Profile.module.css';
 
 const ProfileForm = () => {
+  const [newPassword, setNewPassword] = useState('');
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDddUDuA6N4zFOCqXbkYxSOpZJt-q1HHcQ`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            idToken: authCtx.token,
+            newPassword: newPassword,
+            returnSecureToken: true,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to change password');
+      }
+
+      console.log('Password changed');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const newPasswordChangeHandler = (event) => {
+    setNewPassword(event.target.value);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input
+          type='password'
+          id='new-password'
+          value={newPassword}
+          onChange={newPasswordChangeHandler}
+        />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button type='submit'>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
+
+
+
+
