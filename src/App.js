a210e91c1b76c848c085e7a66aae3a720cc7d576
piyush <sainipiyus11111@gmail.com>
2023-07-19 08:52:06 +0530

    navigate to a new paeg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthForm from './component/LogInPage';
+import AboutUs from './component/AbooutUs';
 
 const App = () => {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+  };
 
   return (
-    <div>
-      <AuthForm/>
-          </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <Navigate to="/about-us" /> : <AuthForm onLogin={loginHandler} />}
+        />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </Router>
   );
 };
 
