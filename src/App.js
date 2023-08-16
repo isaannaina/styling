f06cc5c2f85b650159c290ecbce3370ad37013fc
piyush <sainipiyus11111@gmail.com>
2023-08-16 17:06:10 +0530

    inbox view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import SignUpForm from './component/SignUpForm';
 
-function App() {
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUpForm from './component/SignUpForm';
+import AuthForm from './component/LogInPage';
+import MailPage from './component/EmailPage/MailPage';
+const App = () => {
+  
   return (
-    <div >
-      <SignUpForm />git 
-    </div>
+      <Router>
+        <Routes>
+          <Route
+            path="/login-page"
+            element={<SignUpForm/>}
+          />
+          <Route
+            path="/"
+            element={<AuthForm/>}
+          />
+         <Route
+            path="/about-us"
+            element={<MailPage/>}
+          />
+        </Routes>
+      </Router>
   );
-}
+};
 
 export default App;
+
+
+
+
+
 
