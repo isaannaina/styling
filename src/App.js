f50d26b70be6021d992baf8d858dbc10cecd7428
piyush <sainipiyus11111@gmail.com>
2023-08-16 16:26:04 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Layout from './components/Layout/Layout';
-import Cart from './components/Cart/Cart';
-import Products from './components/Shop/Products';
-import { addToCart } from './Redux/CartSlice';
+import React from 'react';
+import SignUpForm from './component/SignUpForm';
 
 function App() {
-  const cartIsVisible = useSelector((state) => state.cart.cartIsVisible);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    fetch('https://expense-tracker-4e143-default-rtdb.firebaseio.com/expenses.json')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch expenses from the database.');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data) {
-          const expensesArray = Object.keys(data).map((id) => ({
-            id,
-            ...data[id],
-          }));
-
-      
-          expensesArray.forEach((expense) => {
-            dispatch(addToCart(expense));
-          });
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching expenses:', error.message);
-      });
-  }, [dispatch]);
-
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <div >
+      <SignUpForm />
+    </div>
   );
 }
 
 export default App;
+
